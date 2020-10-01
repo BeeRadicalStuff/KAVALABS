23b1ba0d5a94f2435b343768bc5e58579a1d8eb5
rhuairahrighairigh <ruaridh.odonnell@gmail.com>
2020-10-01 21:51:11 +0100

    add pricefeed test with expired prices

--- x/pricefeed/genesis_test.go
+++ x/pricefeed/genesis_test.go
@@ -2,13 +2,14 @@ package pricefeed_test
 
 import (
 	"testing"
+	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/suite"
+	abci "github.com/tendermint/tendermint/abci/types"
 
 	"github.com/kava-labs/kava/app"
 	"github.com/kava-labs/kava/x/pricefeed"
-
-	"github.com/stretchr/testify/suite"
 )
 
 type GenesisTestSuite struct {
@@ -33,6 +34,52 @@ func (suite *GenesisTestSuite) TestValidGenState() {
 			NewPricefeedGenStateWithOracles(addrs),
 		)
 	})
+}
+
+func (suite *GenesisTestSuite) TestPostPriceAfterInitGenesis() {
+	_, oracles := app.GeneratePrivKeyAddressPairs(10)
+	genPriceExpiry := time.Date(1998, 1, 1, 0, 0, 0, 0, time.UTC)
+
+	pfGenesis := pricefeed.GenesisState{
+		Params: pricefeed.Params{
+			Markets: []pricefeed.Market{
+				{MarketID: "btc:usd", BaseAsset: "btc", QuoteAsset: "usd", Oracles: oracles, Active: true},
+			},
+		},
+		PostedPrices: []pricefeed.PostedPrice{
+			{
+				MarketID:      "btc:usd",
+				OracleAddress: oracles[0],
+				Price:         sdk.MustNewDecFromStr("8000.00"),
+				Expiry:        genPriceExpiry,
+			},
+			{
+				MarketID:      "btc:usd",
+				OracleAddress: oracles[1],
+				Price:         sdk.MustNewDecFromStr("8000.00"),
+				Expiry:        genPriceExpiry.Add(2 * time.Nanosecond),
+			},
+		},
+	}
+
+	tApp := app.NewTestApp()
+	genesisTime := genPriceExpiry.Add(1 * time.Nanosecond)
+	tApp.InitializeFromGenesisStatesWithTime(
+		genesisTime,
+		app.GenesisState{pricefeed.ModuleName: pricefeed.ModuleCdc.MustMarshalJSON(pfGenesis)},
+	)
+	keeper := tApp.GetPriceFeedKeeper()
+
+	ctx := tApp.NewContext(false, abci.Header{Time: genesisTime.Add(1 * time.Hour)})
+	_, err := keeper.SetPrice(
+		ctx,
+		oracles[0],
+		"btc:usd",
+		sdk.MustNewDecFromStr("9000.00"),
+		genPriceExpiry.Add(11*time.Hour),
+	)
+
+	suite.Require().NoError(err)
 }
 
 func TestGenesisTestSuite(t *testing.T) {
